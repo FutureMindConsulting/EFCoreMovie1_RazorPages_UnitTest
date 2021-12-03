c5c9b21b1245bbe7cde27faf8fb220984c0b687b
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2021-12-03 14:15:01 +0100

    Removed password

--- EFCoreMovie1_RazorPages/Models/MovieDBContext.cs
+++ EFCoreMovie1_RazorPages/Models/MovieDBContext.cs
@@ -26,7 +26,7 @@ namespace EFCoreMovie1_RazorPages.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=mssql12.unoeuro.com;Initial Catalog=futuremind_nu_db_test;Persist Security Info=True;User ID=futuremind_nu;Password=Vandglas26");
+                optionsBuilder.UseSqlServer("Data Source=mssql12.unoeuro.com;Initial Catalog=futuremind_nu_db_test;Persist Security Info=True;User ID=futuremind_nu;Password=<Your PassWord>");
             }
         }
 
